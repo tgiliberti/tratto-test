705727fff0f6631ca77975fdf2a5500fb5661095
Daniel Carvalho <danielbpc2@hotmail.com>
2018-06-12 08:00:51 -0700

    add some stuff to wallet class, first thoughts on the class

--- lib/wallet.rb
+++ lib/wallet.rb
@@ -1,2 +1,17 @@
 class Wallet
+  def initialize(client, currency, amount)
+    @client = client
+    @currency = currency
+    @amount = amount
+  end
+
+
+private
+
+  def load
+  end
+
+  def save
+  end
+
 end
