b63e2e693d6c8d184486b3350e2f3527651b826f
Daniel Carvalho <danielbpc2@hotmail.com>
2018-06-12 11:55:40 -0700

    load and save methods sketch and start of wallet central

--- lib/wallet.rb
+++ lib/wallet.rb
@@ -1,17 +1,38 @@
+require 'csv'
+  # all wallets
+  WALLETS = []
+
 class Wallet
+  attr_reader :client, :currency, :amount
+  attr_writer :amount
   def initialize(client, currency, amount)
     @client = client
     @currency = currency
     @amount = amount
   end
 
-
-private
-
-  def load
+  private
+  # Method to load CSV wallets.
+  def self.load
+    #
+    filepath = 'resources/wallets.csv'
+    #
+    csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+    CSV.foreach(filepath, csv_options) do |row|
+    loaded = Wallet.new(row['Client'], row['Currency'], row['Amount'])
+    WALLETS << loaded
+    end
+    return WALLETS
   end
-
-  def save
+  # Method to save new csv
+  def self.save
+    filepath    = 'resources/wallets.csv'
+    csv_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
+    CSV.open(filepath, 'wb', csv_options) do |csv|
+      csv << ['Client', 'Currency', 'Amount']
+      WALLETS.each do |wallet|
+        csv << [wallet.client, wallet.currency, wallet.amount]
+      end
+    end
   end
-
 end
--- lib/wallet_central.rb
+++ lib/wallet_central.rb
@@ -1,2 +1,15 @@
+require_relative 'wallet'
+
 class WalletCentral
+
+  def initialize
+    Wallet.load
+  end
+
+  def transfer(wallet1, wallet2, currency, amount_transfered)
+    wallet1.amount -= amount_transfered
+
+  end
+
 end
+
