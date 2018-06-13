ba8c68442016c6e311bba31d4f7d5f079282b956
Daniel Carvalho <danielbpc2@hotmail.com>
2018-06-13 13:40:51 -0700

    wallet json output creator kinda done

--- lib/wallet.rb
+++ lib/wallet.rb
@@ -1,5 +1,7 @@
 require 'csv'
-  # all wallets
+require 'json'
+require 'pry'
+  # all wallets array
   WALLETS = []
 
 class Wallet
@@ -9,9 +11,11 @@ class Wallet
     @client = client
     @currency = currency
     @amount = amount
+    WALLETS << self
   end
 
   private
+
   # Method to load CSV wallets.
   def self.load
     #
@@ -19,11 +23,37 @@ class Wallet
     #
     csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
     CSV.foreach(filepath, csv_options) do |row|
-    loaded = Wallet.new(row['Client'], row['Currency'], row['Amount'])
-    WALLETS << loaded
+    Wallet.new(row['Client'], row['Currency'], row['Amount'])
     end
-    return WALLETS
   end
+
+  def self.json_output
+    # create a json filepath
+    filepath = 'resources/wallets_output.json'
+    # wallets that will be written in json format
+    output_wallets = []
+
+    WALLETS.each_with_index do |wallet, index|
+     # separate the wallets with the same client as the one in the current loop
+      same_name = WALLETS.select { |x| wallet.client == x.client }
+      # save the client name in the output and create a hash wallets
+      output_wallets << {
+                            name: "#{wallet.client}",
+                            "wallets": {}
+                          }
+      # grab the wallets that has the same client and store their wallets in the empty hash
+      same_name.each do |x|
+        output_wallets[index][:wallets].store("#{x.currency}", "#{x.amount}")
+      end
+    end
+    # remove duplicates in the output
+    output_wallets.uniq!
+    # generate json
+    File.open(filepath, 'wb') do |file|
+      file.write(JSON.generate(output_wallets))
+    end
+  end
+
   # Method to save new csv
   def self.save
     filepath    = 'resources/wallets.csv'
@@ -36,3 +66,4 @@ class Wallet
     end
   end
 end
+
--- lib/wallet_central.rb
+++ lib/wallet_central.rb
@@ -7,8 +7,6 @@ class WalletCentral
   end
 
   def transfer(wallet1, wallet2, currency, amount_transfered)
-    wallet1.amount -= amount_transfered
-
   end
 
 end
